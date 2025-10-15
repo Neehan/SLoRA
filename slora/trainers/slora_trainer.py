b89ee0bf413d30bb4555dfeb98c0406783653ca2
Adib Hasan <nzxcvb5@gmail.com>
2025-10-15 00:13:45 -0400

    loss scaling

--- slora/trainers/slora_trainer.py
+++ slora/trainers/slora_trainer.py
@@ -125,7 +125,11 @@ class SLoRATrainer(Trainer):
             loss = loss / self.args.gradient_accumulation_steps
 
         if accept:
-            self.accelerator.backward(loss)
+            # since we reject some samples, scale the loss to preserve adam's moments
+            scale_factor = 1 / max(
+                1e-3, self.gate.acceptance_rate(self.state.global_step)
+            )
+            self.accelerator.backward(loss * scale_factor)
             self.gate.update(z, count_increment)
 
         # don't cheat by reporting losses for both accepted and rejected batches
